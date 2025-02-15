f57048a1bf711711aa2ba7bc458d9c09930a3e06
doris Cruz <dbcruzhdez@gmail.com>
2025-02-14 23:21:29 -0800

    Se agrego Gui_Dinamico

---
+++ GUI_Dinamica/Imagenes.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace VisualizadorDeImagenes
+{
+    partial class Imagenes
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.btnImagenes = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(305, 113);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(144, 20);
+            this.txtDireccion.TabIndex = 0;
+            // 
+            // btnImagenes
+            // 
+            this.btnImagenes.Location = new System.Drawing.Point(470, 110);
+            this.btnImagenes.Name = "btnImagenes";
+            this.btnImagenes.Size = new System.Drawing.Size(75, 23);
+            this.btnImagenes.TabIndex = 1;
+            this.btnImagenes.Text = "Ver";
+            this.btnImagenes.UseVisualStyleBackColor = true;
+            this.btnImagenes.Click += new System.EventHandler(this.btnImagenes_Click_1);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(21, 148);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(758, 290);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(273, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(225, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Introduzca la direccion de la carpeta:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Mongolian Baiti", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(272, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(249, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "GALERIA DE IMAGENES";
+            // 
+            // Imagenes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(246)))), ((int)(((byte)(251)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.btnImagenes);
+            this.Controls.Add(this.txtDireccion);
+            this.Name = "Imagenes";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.Button btnImagenes;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ GUI_Dinamica/Imagenes.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace VisualizadorDeImagenes
+{
+    public partial class Imagenes : Form
+    {
+        public Imagenes()
+        {
+            InitializeComponent();
+        }
+
+
+        private void CargarImagenes(string directoryPath)
+        {
+            flowLayoutPanel1.Controls.Clear();
+            string[] imageFiles = Directory.GetFiles(directoryPath, "*.*")
+                .Where(s => s.EndsWith(".png") || s.EndsWith(".jpg") || s.EndsWith(".jpeg") || s.EndsWith(".gif"))
+                .ToArray();
+
+            if (imageFiles.Length == 0)
+            {
+                MessageBox.Show("No se encontraron imágenes en ese directorio.");
+                return;
+            }
+
+            foreach (string imagePath in imageFiles)
+            {
+                try
+                {
+                    PictureBox picBox = new PictureBox();
+                    Image img = Image.FromFile(imagePath);
+                    picBox.Image = img.GetThumbnailImage(100, 100, null, IntPtr.Zero); 
+                    picBox.SizeMode = PictureBoxSizeMode.StretchImage;
+                    picBox.Width = 100;
+                    picBox.Height = 100;
+                    picBox.Click += (s, e) => AbrirImagenes(imagePath);
+                    flowLayoutPanel1.Controls.Add(picBox);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("No se puedo cargar la imagen: " + ex.Message);
+                }
+
+            }
+        }
+            private void AbrirImagenes(string imagePath)
+        {
+            Form ventanaImagen = new Form();
+            ventanaImagen.Text = Path.GetFileName(imagePath);
+            PictureBox picBox = new PictureBox();
+            picBox.Image = Image.FromFile(imagePath);
+            picBox.SizeMode = PictureBoxSizeMode.AutoSize;
+            ventanaImagen.Controls.Add(picBox);
+            ventanaImagen.ShowDialog();
+        }
+
+        private void btnImagenes_Click_1(object sender, EventArgs e)
+        {
+            string direccion = txtDireccion.Text;  
+            if (Directory.Exists(direccion))
+            {
+                CargarImagenes(direccion);
+            }
+            else
+            {
+                MessageBox.Show("El directorio especificado no existe. Por favor, introduce una ruta válida.");
+            }
+        }
+    }
+}
